984f0606368937e2fd2c5716c931b3bae0b51acf
homahi <homahi0128@gmail.com>
2017-09-21 18:25:53 +0900

    ベットの特性を返答できるようにした

--- examples/slack_bot.js
+++ examples/slack_bot.js
@@ -80,20 +80,73 @@ var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
 
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['みかちゃんは何者？'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, '美少女だよ')
+});
+
+controller.hears(['本当のみかちゃんは？'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, '28歳無職のアニオタだよ……')
+})
+
+controller.hears(['メッセージベット'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, '発展性に乏しいハンドを持っているときに、相手のドローハンドのオッズを削るために打たれるベット。打たないことが不利益になる場合に、消極的に打つことが多い性格のベットで、ほとんどの場合はフラットな立場で打つ。')
+});
+
+controller.hears(['プロテクションベット'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, 'トップヒットしたことなど、正しいストーリーを相手に伝えるためのベット。ただし相手に勝っている自信のもとに打たれるとは限らない。例えばフロップで高めのミドルヒットをした場合や、低めのトップヒットをした場合など、ヒットしたことを空いてに伝えるため')
+});
+
+controller.hears(['インシステントベット'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, '突然ウェットなボードになったときなどに、嘘のストーリーを相手に信じさせるためのベット。いわゆるブラフベットであることが多いが、必ずしも嘘のヒットを主張するわけではなく、嘘のスリックを主張することも多い。強いヒットをしたのにわざと少額のベットをすることで、「滑ったから安いブラフをしている」と信じさせてバリューを引き出すようなインシステントベットもある。')
+});
+
+controller.hears(['ブロックベット'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, 'ベットはしたいがレイズはできないというような相手に対して、先んじて少額のベットすることなどで、相手のアクションを制限してポットサイズを小さく保つためのベット。')
+});
+
+controller.hears(['アグレッションベット'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, 'ポジションがあるフロップにおいて、ターンチェックバックから、リバーでフリードローする狙いで打たれるフロップCBなど、アグレッションを獲得または保持することで、アグレッションによる優位性を活かすためのベット。')
+});
+
+controller.hears(['リスクベット'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, 'モノトーンフロップのオーバーベットや、ボードストレート時のビッグベットなど、ブラフだと見破られたとしてもリスクの大きさを根拠にフォールドを迫るためのベット。極端に大きなベットでなくても、早いストリートにおけるブラフベットは、たいてい十分なストーリーを補完できないので、インシステントベットというよりリスクベットの性格が強くなる。')
+});
+
+controller.hears(['アイソレートベット'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, 'フロップマルチウェイにおけるポットサイズCBなど、主に参加者を減らす目的で打たれるベット。多くは漫然と人数を減らすために打たれるが、2オーバーを降ろしてトップヒットだけを残すことで、ポストフロップにおけるオーバーカードを、相手にとって確実にスケアとする目的などでも打たれる。')
+});
+
+controller.hears(['プッシュベット'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, 'ポットを膨らませるためのベット。大きなポットにリークがあるプレイヤーを狩ったり、レンジvsレンジなど難しいヘッズアップで、ポストフロップは有利にプレイできるという自信に基づき、強弱の曖昧なフロップ時点からポットを膨らませておくようなベット。')
+});
+
+controller.hears(['セッティングベット'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, 'ライト3ベッターに対してタイトにオープンするなど、特定の状況をセッティングするためのベット。おかしな額のベットを標準サイズに直して、事故の起こりやすい特殊な状況を避けるためにもおこなわれる。')
+});
+
+controller.hears(['スチールベット'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, 'オーバーフォールドする相手に対しておこなわれるような、スチールを狙うベット。オーバーフォールドする相手でなくとも、ポジションやアグレッションなどの理由から、フォールドせざるをえない相手に対してもおこなわれる。ストーリーがなくても、額が大きくなくても、相手のフォールドが期待できるという特徴がある。')
+});
+
+controller.hears(['ドミネイトベット'], 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, 'ナッツあるいはドミネイトハンドを持っているときに、アンダードッグにコールさせるためのベット。もっとも基本的なベットだが、ナッツあるいはドミネイト判断が難しいため、案外純粋なバリューベットが打てる機会は少ない。');
+});
+
+
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function (bot, message) {
 
     bot.api.reactions.add({
         timestamp: message.ts,
         channel: message.channel,
         name: 'robot_face',
-    }, function(err, res) {
+    }, function (err, res) {
         if (err) {
             bot.botkit.log('Failed to add emoji reaction :(', err);
         }
     });
 
 
-    controller.storage.users.get(message.user, function(err, user) {
+    controller.storage.users.get(message.user, function (err, user) {
         if (user && user.name) {
             bot.reply(message, 'Hello ' + user.name + '!!');
         } else {
@@ -102,35 +155,34 @@ controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', funct
     });
 });
 
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
     var name = message.match[1];
-    controller.storage.users.get(message.user, function(err, user) {
+    controller.storage.users.get(message.user, function (err, user) {
         if (!user) {
             user = {
                 id: message.user,
             };
         }
         user.name = name;
-        controller.storage.users.save(user, function(err, id) {
+        controller.storage.users.save(user, function (err, id) {
             bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
         });
     });
 });
 
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function (bot, message) {
 
-    controller.storage.users.get(message.user, function(err, user) {
+    controller.storage.users.get(message.user, function (err, user) {
         if (user && user.name) {
             bot.reply(message, 'Your name is ' + user.name);
         } else {
-            bot.startConversation(message, function(err, convo) {
+            bot.startConversation(message, function (err, convo) {
                 if (!err) {
                     convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
+                    convo.ask('What should I call you?', function (response, convo) {
+                        convo.ask('You want me to call you `' + response.text + '`?', [{
                                 pattern: 'yes',
-                                callback: function(response, convo) {
+                                callback: function (response, convo) {
                                     // since no further messages are queued after this,
                                     // the conversation will end naturally with status == 'completed'
                                     convo.next();
@@ -138,14 +190,14 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
                             },
                             {
                                 pattern: 'no',
-                                callback: function(response, convo) {
+                                callback: function (response, convo) {
                                     // stop the conversation. this will cause it to end with status == 'stopped'
                                     convo.stop();
                                 }
                             },
                             {
                                 default: true,
-                                callback: function(response, convo) {
+                                callback: function (response, convo) {
                                     convo.repeat();
                                     convo.next();
                                 }
@@ -154,20 +206,22 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
 
                         convo.next();
 
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
+                    }, {
+                        'key': 'nickname'
+                    }); // store the results in a field called nickname
 
-                    convo.on('end', function(convo) {
+                    convo.on('end', function (convo) {
                         if (convo.status == 'completed') {
                             bot.reply(message, 'OK! I will update my dossier...');
 
-                            controller.storage.users.get(message.user, function(err, user) {
+                            controller.storage.users.get(message.user, function (err, user) {
                                 if (!user) {
                                     user = {
                                         id: message.user,
                                     };
                                 }
                                 user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
+                                controller.storage.users.save(user, function (err, id) {
                                     bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
                                 });
                             });
@@ -186,43 +240,43 @@ controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention
 });
 
 
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function (bot, message) {
 
-    bot.startConversation(message, function(err, convo) {
+    bot.startConversation(message, function (err, convo) {
 
-        convo.ask('Are you sure you want me to shutdown?', [
-            {
+        convo.ask('Are you sure you want me to shutdown?', [{
                 pattern: bot.utterances.yes,
-                callback: function(response, convo) {
+                callback: function (response, convo) {
                     convo.say('Bye!');
                     convo.next();
-                    setTimeout(function() {
+                    setTimeout(function () {
                         process.exit();
                     }, 3000);
                 }
             },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
+            {
+                pattern: bot.utterances.no,
+                default: true,
+                callback: function (response, convo) {
+                    convo.say('*Phew!*');
+                    convo.next();
+                }
             }
-        }
         ]);
     });
 });
 
 
 controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
-    'direct_message,direct_mention,mention', function(bot, message) {
+    'direct_message,direct_mention,mention',
+    function (bot, message) {
 
         var hostname = os.hostname();
         var uptime = formatUptime(process.uptime());
 
         bot.reply(message,
             ':robot_face: I am a bot named <@' + bot.identity.name +
-             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+            '>. I have been running for ' + uptime + ' on ' + hostname + '.');
 
     });
 
